7f4786e4580ce88dbfec23af0de7eec9c233990e
Grinvincible_f <ly_cwx@163.com>
2023-02-25 15:50:49 +0800

    first commit

---
+++ js02.js
@@ -0,0 +1,5 @@
+function changeLink(){
+    document.getElementById('demo2').innerHTML="Youtube!"            
+    document.getElementById('demo2').href="https://www.youtube.com/"            
+    document.getElementById('demo2').target="_blank"            
+}
